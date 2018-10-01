a5b873a28788eec5d1bf860b3d2a42ef01b55f0b
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2018-09-30 21:24:34 -0700

    Update emit.js

--- emit.js
+++ emit.js
@@ -1,4 +1,6 @@
 var x = event.clientX;     // Get the horizontal coordinate
 var y = event.clientY;     // Get the vertical coordinate
 var coor = "X coords: " + x + ", Y coords: " + y;
+document.getElementById("coord").onclick = function(){
 document.getElementById("coord").innerHTML = coor;
+}
