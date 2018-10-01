357c02551765e23ca86ee97c74bae6f1cc7c8640
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2018-09-30 21:23:44 -0700

    Create emit.js

---
+++ emit.js
@@ -0,0 +1,4 @@
+var x = event.clientX;     // Get the horizontal coordinate
+var y = event.clientY;     // Get the vertical coordinate
+var coor = "X coords: " + x + ", Y coords: " + y;
+document.getElementById("coord").innerHTML = coor;
