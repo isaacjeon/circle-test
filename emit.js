d80a0ec35aa09c988e9d83442c35530cde7fe19f
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2018-09-30 23:30:58 -0700

    Update emit.js

--- emit.js
+++ emit.js
@@ -1,7 +1,9 @@
 var c = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-c.style.width  = window.innerWidth;
+c.style.width = window.innerWidth;
 c.style.height = window.innerHeight;
+
+var colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+var count = 0;
 
 function emit() {
     var pos = getCenterPos();
@@ -9,8 +11,13 @@ function emit() {
     var id = setInterval(frame, 10);
     x = pos.x;
     y = pos.y;
-    ctx.strokeStyle = "red";
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = colors[count];
     function frame() {
+        if (rad == 10) {
+            count %= 7;
+            emit();
+        }
         if (pos == 100) {
           clearInterval(id);
         } else {
@@ -25,8 +32,8 @@ function emit() {
 function getCenterPos() {
     var rect = c.getBoundingClientRect();
     return {
-      x: rect.width - rect.left,
-      y: rect.height - rect.top
+      x: (rect.width - rect.left)/2,
+      y: (rect.height - rect.top)/2
     };
 }
 
