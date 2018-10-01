a3f8a744ebbb26b2e1e33e0acdd4011bedc6dc33
Isaac Jeon <43486715+isaacjeon@users.noreply.github.com>
2018-09-30 22:13:57 -0700

    Update emit.js

--- emit.js
+++ emit.js
@@ -1,8 +1,22 @@
-document.addEventListener("click", function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-});
-var coor = "X coords: " + x + ", Y coords: " + y;
-document.getElementById("coord").onclick = function(){
-document.getElementById("coord").innerHTML = coor;
+var c = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function draw(e) {
+    var pos = getMousePos(c, e);
+    x = pos.x;
+    y = pos.y;
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2*Math.PI);
+    ctx.stroke();
 }
+
+function getMousePos(c, e) {
+    var rect = c.getBoundingClientRect();
+    return {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
+}
+
+c.onclick = function(){draw(e)};
